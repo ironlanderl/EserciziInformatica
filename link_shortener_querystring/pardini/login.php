6da06312d4470972249ef4e22cc5ba02c2c996b9
Fabrizio Pardini <fabrizio03p@gmail.com>
2022-04-12 08:27:28 +0200

    Login Fixes

--- link_shortener_querystring/pardini/login.php
+++ link_shortener_querystring/pardini/login.php
@@ -1,15 +1,45 @@
 <?php
 // Import funzioni
 include_once "dbUtils.php";
-include_once "httest.php";
 
 // Start the session
 session_start();
 
 //Check if logged in
-if ($_SESSION){
+if ($_SESSION) {
     // Redirect
     header("Location: index.php");
+}
+
+// Controllo se è presente una richiesta post
+if ($_POST) {
+    // Prendo il nome utente e password dalla richiesta
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    /* Parte del login al DB */
+    $conn = connetti();
+
+    /**********************************/
+
+    // Scarico la password hashata
+    $query = "SELECT * FROM users WHERE username = '" . strip_tags(htmlspecialchars($username) . "'");
+    $result = mysqli_query($conn, $query);
+
+    disconnetti($conn);
+
+    if ($result->num_rows > 0) {
+        // È presente un elemento
+        // Estraggo la password
+        $hashpass = $result->fetch_assoc()["password"];
+        // Controllo se la password è giusta
+        if (password_verify($password, $hashpass) == 1) {
+            $_SESSION["user"] = $username;
+            header("Location: index.php");
+            return;
+        }
+    }
+    $errore = true;
 }
 
 ?>
@@ -17,53 +47,21 @@ if ($_SESSION){
 <html>
 
 <body>
-    <?php
-    // Controllo se è presente una richiesta post
-    if ($_POST) {
-        // Prendo il nome utente e password dalla richiesta
-        $username = $_POST["username"];
-        $password = $_POST["password"];
 
-        /* Parte del login al DB */
-        $conn = connetti();
-        
-        /**********************************/
-
-        // Scarico la password hashata
-        $query = "SELECT * FROM users WHERE username = '" . strip_tags(htmlspecialchars($username) . "'");
-        $result = mysqli_query($conn, $query);
-
-        disconnetti($conn);
-
-        if ($result->num_rows > 0){
-            // È presente un elemento
-            // Estraggo la password
-            $hashpass = $result->fetch_assoc()["password"];
-            // Controllo se la password è giusta
-            if (password_verify($password, $hashpass) == 1){
-                $_SESSION["user"] = $username;
-                header("Location: index.php");
-                return;
-            }
-        }
-        echo "Nome utente o password errati";
-        
-    }
-
-    StampaForm();
-
-    
-    function StampaForm()
-    {
-        echo
-        '<div class="modal modal-signin position-static d-block py-5" tabindex="-1" role="dialog" id="modalSignin">
+        <div class="modal modal-signin position-static d-block py-5" tabindex="-1" role="dialog" id="modalSignin">
             <div class="modal-dialog" role="document">
                 <div class="modal-content rounded-5 shadow">
+                    <?php
+                    if ($errore) echo '
+                    <div class="p-5 pb-1 border-bottom-0">
+                        <h4 class="mb-0">Nome utente o password errati</h4>
+                    </div>
+                    ';?>
                     <div class="modal-header p-5 pb-4 border-bottom-0">
                         <h2 class="fw-bold mb-0">Login</h2>
                     </div>
                     <div class="modal-body p-5 pt-0">
-                        <form class="" action=' . $_SERVER["PHP_SELF"] . ' method="post">
+                        <form class="" action=<?php echo $_SERVER["PHP_SELF"];?> method="post">
                             <div class="form-floating mb-3">
                                 <input type="text" class="form-control rounded-4" id="floatingInput" placeholder="username" name="username" required>
                                 <label for="floatingInput">Nome Utente</label>
@@ -78,9 +76,7 @@ if ($_SESSION){
                     </div>
                 </div>
             </div>
-        </div>';
-    }
-    ?>
+        </div>
 
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
